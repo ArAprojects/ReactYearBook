3448e63be15b9af83cdcf7f6ef245d51286fd4eb
Alek <alekaker@outlook.com>
2019-08-12 16:25:06 -0600

    add cohort component

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Person from './Person';
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+const Cohort = (props) => {
+  console.log(props)
+  return <div> hey hey </div>;
+};
 
 export default Cohort;
